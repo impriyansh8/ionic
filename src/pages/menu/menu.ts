4063dd3fee2c27cce62ff660429f92e9e03b75ff
Priyansh Bhardwaj <priyanshbhj@gmail.com>
2018-06-01 17:33:35 +0530

    Ionic Navigation

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Dish } from '../../shared/dish';
+import { DishProvider } from '../../providers/dish/dish';
+import { DishdetailPage } from '../dishdetail/dishdetail';
 
 /**
  * Generated class for the MenuPage page.
@@ -14,12 +17,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'menu.html',
 })
 export class MenuPage {
+  dishes: Dish[];
+  errMess: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private dishService: DishProvider,
+    @Inject('BaseURL') public BaseURL) { }
+
+  ngOnInit() {
+    this.dishService.getDishes()
+      .subscribe(dishes => this.dishes = dishes,
+      errmess => this.errMess = <any>errmess);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
   }
 
+  dishSelected(event, dish) {
+    this.navCtrl.push(DishdetailPage, {
+      dish: dish
+    });
+  }
 }
